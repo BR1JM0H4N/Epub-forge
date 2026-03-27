80c4675727e402f51c0060f871415c89028756d9
Br1jm0h4n <158831034+BR1JM0H4N@users.noreply.github.com>
2026-03-27 17:05:42 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'v3';
+const CACHE_VERSION = 'v4.1';
 const STATIC_CACHE = `epub-forge-static-${CACHE_VERSION}`;
 const RUNTIME_CACHE = `epub-forge-runtime-${CACHE_VERSION}`;
 
